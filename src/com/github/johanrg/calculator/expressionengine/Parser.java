32697bc9e9bbad39ed0f4c2d1c94a2249f97ccf3
johanrg <johanrg@github.com>
2016-05-29 21:49:46 +0200

    Changed how the parser handles end of string

--- src/com/github/johanrg/calculator/expressionengine/Parser.java
+++ src/com/github/johanrg/calculator/expressionengine/Parser.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 class Parser {
     private final String text;
-    private int charsRemaining;
     private int pos = 0;
     private int line = 1;
     private int column = 1;
@@ -32,50 +31,42 @@ class Parser {
         return (c == ' ' || c == '\t' || c == '\r' || isNewLine(c));
     }
 
-    private boolean isAValidStopForNumber(char c) {
-        return isWhitespace(c) || c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == ')';
-    }
-
     private char peekAtChar() {
         return text.charAt(pos);
     }
 
     private void eatTheChar() {
         ++pos;
-        --charsRemaining;
         ++column;
     }
 
-    private boolean hasNext() {
-        return charsRemaining > 0;
+    private char getAndEatTheChar() {
+        char c = peekAtChar();
+        eatTheChar();
+        return c;
     }
 
     private void eatAllWhitespaces() {
-        while (hasNext() && isWhitespace(peekAtChar())) {
-            if (isNewLine(peekAtChar())) {
+        char c = peekAtChar();
+        while (isWhitespace(c)) {
+            if (isNewLine(c)) {
                 column = 0;
                 ++line;
             }
             eatTheChar();
+            c = peekAtChar();
         }
     }
 
     private Token getNumber(boolean positiveNumber) throws ParserException {
         StringBuilder number = new StringBuilder();
 
-        do {
-            char c = peekAtChar();
-
-            if (isNumber(c) || c == '.') {
-
-                number.append(c);
-            } else if (isAValidStopForNumber(c) && number.length() > 0) {
-                break;
-            } else {
-                throw new ParserException(String.format("Expected a number on line %d column %d got '%c'", line, column, c));
-            }
+        char c = peekAtChar();
+        while (isNumber(c) || c == '.') {
+            number.append(c);
             eatTheChar();
-        } while (hasNext());
+            c = peekAtChar();
+        }
 
         if (positiveNumber) {
             return new Token(TokenType.NUMBER, Double.parseDouble(number.toString()));
@@ -90,55 +81,51 @@ class Parser {
     }
 
     private void parse() throws ParserException {
-        while (hasNext()) {
-            char c = peekAtChar();
+        for (; ; ) {
+            char c = getAndEatTheChar();
             if (isWhitespace(c)) {
                 eatAllWhitespaces();
 
             } else if (c == '+') {
-                eatTheChar();
                 boolean mustBeOperator = (lastToken == TokenType.NUMBER);
-                if (!mustBeOperator && hasNext() && isNumber(peekAtChar())) {
+                if (!mustBeOperator && isNumber(peekAtChar())) {
                     addToken(getNumber(true));
                 } else {
                     addToken(new Token(TokenType.PLUS));
                 }
 
             } else if (c == '-') {
-                eatTheChar();
                 boolean mustBeOperator = (lastToken == TokenType.NUMBER);
-                if (!mustBeOperator && hasNext() && isNumber(peekAtChar())) {
+                if (!mustBeOperator && isNumber(peekAtChar())) {
                     addToken(getNumber(false));
                 } else {
                     addToken(new Token(TokenType.MINUS));
                 }
 
             } else if (c == '*') {
-                eatTheChar();
                 addToken(new Token(TokenType.MULTIPLY));
 
             } else if (c == '/') {
-                eatTheChar();
                 addToken(new Token(TokenType.DIVIDE));
 
             } else if (c == '%') {
-                eatTheChar();
                 addToken(new Token(TokenType.MODULUS));
 
             } else if (c == '^') {
-                eatTheChar();
                 addToken(new Token(TokenType.CARET));
 
             } else if (c == '(') {
-                eatTheChar();
                 addToken(new Token(TokenType.OPEN_PARENTHESES));
 
             } else if (c == ')') {
-                eatTheChar();
                 addToken(new Token(TokenType.CLOSE_PARENTHESES));
 
             } else if (isNumber(c)) {
                 addToken(getNumber(true));
+
+            } else if (c == '\0') {
+                break;
+
             } else {
                 throw new ParserException(String.format("Syntax error: '%c' on line %d column %d", c, line, column));
             }
@@ -146,8 +133,10 @@ class Parser {
     }
 
     Parser(String text) throws ParserException {
-        this.text = text;
-        this.charsRemaining = text.length();
+        // The added 0 to the end of the string makes us not having to test the if the string has reached it's end
+        // everywhere. A valid character will always have at least a 0 that follows after it and this can be checked in
+        // one place and only when no other valid characters are left.
+        this.text = text + "\0";
 
         parse();
 
